56eb0988a75c49d762664a77007e15a8c7b3599b
J.P MICHEL <jpmichelperso@live.fr>
2026-09-02 10:06:32 +0200

    ajout code développeur

--- tests/main.py
+++ tests/main.py
@@ -1,5 +1,5 @@
 # main.py — AllRoad’s API
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI(
     title="AllRoad’s API",
@@ -7,22 +7,33 @@ app = FastAPI(
     version="1.0.0"
 )
 
+# Code développeur temporaire (à changer par ton code perso)
+CODE_DEV = "3647"
+
+
 @app.get("/")
 def home():
     return {"message": "Bienvenue sur AllRoad’s API"}
 
+
 @app.get("/route")
-def get_route():
-    return {
-        "route_name": "Ille-sur-Têt → Thuir",
-        "coordinates": [
-            [42.670, 2.620],
-            [42.635, 2.760]
-        ],
-        "distance_km": 12.5,
-        "duration_min": 18,
-        "transport_mode": "car"
-    }
+def get_route(dev: str | None = None):
+    # Accès développeur
+    if dev == CODE_DEV:
+        return {
+            "route_name": "Ille-sur-Têt -> Thuir",
+            "coordinates": [
+                [42.670, 2.620],
+                [42.635, 2.760]
+            ],
+            "distance_km": 12.5,
+            "duration_min": 18,
+            "transport_mode": "car"
+        }
+
+    # Sécurité normale
+    raise HTTPException(status_code=401, detail="Token d'accès requis.")
+
 
 @app.get("/status")
 def status():
